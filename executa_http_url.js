c8c29baf77b1abea95f78b6863df55b46797c902
thefontes <thefontes@gmail.com>
2023-06-22 20:54:11 -0400

    finalização de atividade

---
+++ executa_http_url.js
@@ -0,0 +1,7 @@
+var http= require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write(req.url);
+    res.end();
+}).listen(8081);
